266c882b93d4e8e7ac5d0bd253946acd388e4be1
Purnima Yadav <purnimayadavyaadav@gmail.com>
2025-10-17 14:55:33 +0530

    left side card completed

--- src/components/RightCard.jsx
+++ src/components/RightCard.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const RightCard = () => {
   return (
-    <div className='h-full w-80 rounded-4xl'>
+    <div className='h-full overflow-hidden relative w-80 rounded-4xl'>
        <img className='h-full w-full object-cover' src="https://media.istockphoto.com/id/1746066315/photo/portrait-of-a-businesswoman-standing-in-the-office.jpg?s=612x612&w=0&k=20&c=YbgR6JbWuqut2dsji9FybRDK6X4YnnJLmMFQjAx9Qqw=" />
+       <div className='absolute top-0 left-0 h-full w-full bg-amber-300'>
+
+       </div>
     </div>
   )
 }
